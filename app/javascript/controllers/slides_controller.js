7be26395ee3d7fa9bfc0a7aa6415449843e25b5a
Metaphysiker <s.raess@me.com>
2022-05-14 15:35:59 +0200

    trying to find a simple navigator

--- app/javascript/controllers/slides_controller.js
+++ app/javascript/controllers/slides_controller.js
@@ -2,12 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 import $ from "jquery"
 
 export default class extends Controller {
-  static targets = [ "input", "output" ]
+  static targets = [ "input", "output", "navigationButtons" ]
 
   connect() {
 
-    this.outputTarget.innerHTML =
-      `Zurück - Weiter`
+    var self = this;
+
+    $.ajax({
+      url: "slides/" + this.inputTarget.innerHTML + ".json",
+      context: document.body
+    }).done(function(response) {
+      self.outputTarget.innerHTML =
+        `${response.content}`
+    });
+
+    this.navigationButtonsTarget.innerHTML =
+      `    <button data-action="click->slides#replace">
+            Greet
+          </button>`
+
+  }
+
+  replace() {
+
+    var self = this;
+
+    $.ajax({
+      url: "slides/" + "2" + ".json",
+      context: document.body
+    }).done(function(response) {
+      self.outputTarget.innerHTML =
+        `${response.content}`
+    });
 
   }
 
--- app/views/slides/_slide.json.jbuilder
+++ app/views/slides/_slide.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! slide, :id, :content, :created_at, :updated_at
 json.url slide_url(slide, format: :json)
+json.next_slide 2
