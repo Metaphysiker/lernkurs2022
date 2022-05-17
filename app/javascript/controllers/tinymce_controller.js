936dfd14dd4be7a088d61ae8f78817d99eddec37
Metaphysiker <s.raess@me.com>
2022-05-17 07:57:14 +0200

    disconnect tinymce

--- app/javascript/controllers/tinymce_controller.js
+++ app/javascript/controllers/tinymce_controller.js
@@ -1,17 +1,27 @@
-// hello_controller.js
-import { Controller } from "@hotwired/stimulus"
 import "tinymce"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "textarea", "css", "output" ]
 
   connect() {
     var self = this;
+    setTimeout(function () {
+        self.initiateTinymce()
+    }, 1000);
+
+  }
+
+  disconnect () {
+    tinymce.remove()
+  }
+
+  initiateTinymce(){
     tinymce.init({
       selector: ".tinymce-textarea",
-      plugins: 'advlist autolink lists link image charmap preview anchor pagebreak code',
+      plugins: 'advlist autolink lists link image charmap preview anchor pagebreak code table',
       menubar: true,
-      toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | code",
+      toolbar: "h1 h2 h3 | formatselect fontselect | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | code styleselect table",
       //toolbar_mode: 'floating',
     })
   }
