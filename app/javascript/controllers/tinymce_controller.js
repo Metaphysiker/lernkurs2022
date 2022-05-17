a242b92e7575bf73422025b62c3fc73b94581fb2
Metaphysiker <s.raess@me.com>
2022-05-17 09:52:32 +0200

    styles instead of styleselect

--- app/javascript/controllers/tinymce_controller.js
+++ app/javascript/controllers/tinymce_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
       selector: ".tinymce-textarea",
       plugins: 'advlist autolink lists link image charmap preview anchor pagebreak code table',
       menubar: true,
-      toolbar: "h1 h2 h3 | formatselect fontselect | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | code styleselect table",
+      toolbar: "styles | h1 h2 h3 | undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | code table",
       //toolbar_mode: 'floating',
     })
   }
