8774e3eabf1f7213cf02440a637c63714c047717
Metaphysiker <s.raess@me.com>
2022-05-21 19:48:38 +0200

    working on animation for percentage people

---
+++ app/javascript/controllers/who_creates_climate_change_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "name", "output" ]
+
+  connect(){
+    import("d3").then(d3 => {
+
+      //d3
+      console.log(d3);
+    })
+  }
+
+  disconnect(){
+    
+  }
+
+  greet() {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
+  }
+}
