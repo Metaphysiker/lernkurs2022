abd4aba676a7a959c1b1180bebfc9e824e1bc55f
Metaphysiker <s.raess@me.com>
2022-05-21 22:40:08 +0200

    working with images works

--- app/javascript/controllers/who_creates_climate_change_controller.js
+++ app/javascript/controllers/who_creates_climate_change_controller.js
@@ -19,65 +19,74 @@ export default class extends Controller {
     let margin = {top: 5, right: 5, bottom: 5, left: 5};
     let width = $(this.outputTarget).width();
     let height = 400;//$(this.outputTarget).height();
+    let text_block = {height: 150, width: 200}
 
     const svg = d3.select(this.outputTarget)
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+    .append("g");
+
+    var starting_position = svg.append("g")
+                            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
+
+    starting_position.append("foreignObject")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("height", height)
+    .attr("width", width)
+  .append("xhtml:div")
+    .html(`<div class="">
+    Wähle eine Bevölkerungsgruppe aus und schau, wie viel Treibhausgase sie verursachen:
+        </div>`);
+
+    var people_richest_1 = starting_position
     .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${width/100 * 0}, ${text_block.height})`);
 
+    people_richest_1
+    .append("text")
+    .text("1%");
 
-
-    var people_richest_1 = svg.append("svg:image")
-    .attr('x', width/100 * 0)
-    .attr('y', 0)
-    .attr('width', 100)
+    people_richest_1
+    .append("svg:image")
+    .attr('width', width/100 * 20)
     .attr('height', 100)
     .style("cursor", "pointer")
     .style("opacity", 1)
     .attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/d/d8/Person_icon_BLACK-01.svg")
-    .style("fill", "#69b3a2");;
+    .style("fill", "#69b3a2");
 
-    var people_richest_10 = svg.append("svg:image")
+    var people_richest_10 = starting_position.append("svg:image")
     .attr('x', width/100 * 25)
-    .attr('y', 0)
-    .attr('width', 100)
+    .attr('y', text_block.height)
+    .attr('width', width/100 * 20)
     .attr('height', 100)
     .style("cursor", "pointer")
     .style("opacity", 1)
     .attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/d/d8/Person_icon_BLACK-01.svg");
 
-    var people_middle_40 = svg.append("svg:image")
+    var people_middle_40 = starting_position.append("svg:image")
     .attr('x', width/100 * 50)
-    .attr('y', 0)
-    .attr('width', 100)
+    .attr('y', text_block.height)
+    .attr('width', width/100 * 20)
     .attr('height', 100)
     .style("cursor", "pointer")
     .style("opacity", 1)
     .attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/d/d8/Person_icon_BLACK-01.svg");
 
-    var people_poorest_50 = svg.append("svg:image")
+    var people_poorest_50 = starting_position.append("svg:image")
     .attr('x', width/100 * 75)
-    .attr('y', 0)
-    .attr('width', 100)
+    .attr('y', text_block.height)
+    .attr('width', width/100 * 20)
     .attr('height', 100)
     .style("cursor", "pointer")
     .style("opacity", 1)
     .attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/d/d8/Person_icon_BLACK-01.svg");
 
-    svg.append("foreignObject")
-    .attr("x", 0)
-    .attr("y", 0)
-    //.attr("width", $("#funky_button1_cta").width())
-    //.attr("height", $("#funky_button1_cta").height())
-    .attr("height", 200)
-    .attr("width", 200)
-  .append("xhtml:div")
-    .html(`<div class="text-center">
-    Wähle eine Bevölkerungsgruppe aus und schau, wie viel Treibhausgase sie verursachen:
-          <button type="button" class="primary-button-a-color">hello!</button>
-        </div>`);
   }
 
   greet() {
