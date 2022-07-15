72a0b3eafc0bc44db105e73872263e09acefff6e
Metaphysiker <s.raess@me.com>
2022-07-15 21:18:32 +0200

    cheat sheet

--- cheat-sheet-stimulus-controller.rb
+++ cheat-sheet-stimulus-controller.rb
@@ -7,3 +7,27 @@ connect(){
     pointsDeductionForMistake = this.pointsDeductionForMistakeTarget.getAttribute('data-value')
   });
 }
+
+#HTML
+
+<div data-controller="quiz">
+  <div data-quiz-target="accountId" data-value="<%= @account.id %>" >
+  </div>
+  <div data-quiz-target="exerciseId" data-value="<%= quiz.id %>" >
+  </div>
+  <div data-quiz-target="exerciseClass" data-value="<%= quiz.class.name %>" >
+  </div>
+  <div data-quiz-target="totalPossiblePoints" data-value="<%= quiz.point_system.total_possible_points unless quiz.point_system.blank? %>" >
+  </div>
+  <div data-quiz-target="pointsDeductionForMistake" data-value="<%= quiz.point_system.points_deduction_for_mistake unless quiz.point_system.blank? %>" >
+  </div>
+  <% quiz.quiz_questions.each do |quiz_question| %>
+  <div class="d-grid gap-2">
+    <button data-quiz-target="button" data-action="click->quiz#check" data-quiz-quiz-question-correct-answer-param="<%= quiz_question.correct_answer %>" data type="button" class="quiz-button "><%= quiz_question.content %></button>
+  </div>
+  <% end %>
+
+
+  <span data-quiz-target="output">
+  </span>
+</div>
