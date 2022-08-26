4befda702603dd485a554c27bbd947a6e80e16a6
Metaphysiker <s.raess@me.com>
2022-08-26 09:02:01 +0200

    added slug

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+gem 'friendly_id', '~> 5.4.0'
 gem 'route_translator'
 
 gem 'devise'
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,9 @@
 class Course < ApplicationRecord
   has_many :slides
 
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   has_one_attached :image do |attachable|
     attachable.variant :cardimage, resize_to_limit: [400, 300]
   end
