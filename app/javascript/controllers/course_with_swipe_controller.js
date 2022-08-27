96aeae82be804e2d542531862ef7f98be29193af
Metaphysiker <s.raess@me.com>
2022-08-27 10:07:53 +0200

    update Corse History of account

--- app/javascript/controllers/course_with_swipe_controller.js
+++ app/javascript/controllers/course_with_swipe_controller.js
@@ -1,7 +1,10 @@
 // hello_controller.js
 import { Controller } from "@hotwired/stimulus"
 
+import * as Ajax from "ajax"
+
 var slider;
+var ajax;
 
 export default class extends Controller {
   static targets = [ "slide", "name", "exercise", "output", "accountId", "courseId", "slideId", "nextSlideId", "previousSlideId", "navigationButtons", "current_slide", "total_slides_count", "prev_button", "next_button" ]
@@ -9,29 +12,32 @@ export default class extends Controller {
 
   connect(){
 
+    ajax = new Ajax.ajax();
+
     var self = this;
+
+    import("jquery_with_setup").then(jquery_with_setup => {
+      ajax.updateCourseHistoryOfAccount(this.accountIdTarget.getAttribute('data-value'), this.courseIdTarget.getAttribute('data-value'), this.slideIdTarget.getAttribute('data-value'))
+    });
 
     import("keen-slider").then(keen_slider => {
 
       console.log(keen_slider);
       slider = new KeenSlider("#my-keen-slider", {
         slideChanged() {
+
+          ajax.updateCourseHistoryOfAccount(this.accountIdTarget.getAttribute('data-value'), this.courseIdTarget.getAttribute('data-value'), slider.track.details.abs);
+
           self.updateNavigationButtons();
-          
+
           console.log('slide changed');
-          console.log(slider.slide);
-          console.log(slider.track.details.abs);
-          console.log(slider.track.details);
+
 
         },
       });
 
     });
 
-    import("jquery_with_setup").then(tinymce_imported => {
-
-      this.updateCourseHistoryOfAccount(this.accountIdTarget.getAttribute('data-value'), this.courseIdTarget.getAttribute('data-value'), this.slideIdTarget.getAttribute('data-value'))
-    });
     //this.getSlide(this.slideIdTarget.getAttribute('data-value'));
 
     // Navigate with arrow buttons
@@ -53,7 +59,7 @@ export default class extends Controller {
     // plus 1 because sort starts with 0
     this.current_slideTarget.textContent = slider.track.details.abs + 1;
 
-    this.next_buttonTarget.classList.add("disabled");
+    //this.next_buttonTarget.classList.add("disabled");
   }
 
   prev() {
