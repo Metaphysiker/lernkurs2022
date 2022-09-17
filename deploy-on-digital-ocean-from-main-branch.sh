60906f6220e0ce3a2232dd268ad9dffd6a9113ba
Metaphysiker <s.raess@me.com>
2022-09-17 16:27:25 +0200

    precompile

---
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rails assets:clobber
+RAILS_ENV=production rails assets:precompile
+git add .
+git commit -m "precompile"
+git push origin main
+
+ssh sandro@159.65.120.231 << EOF
+  cd lernkurs2022
+  git pull origin main
+  bundle config set --local without 'development test'
+  bundle install
+  RAILS_ENV=production rails db:migrate
+EOF
+
+ssh root@159.65.120.231 << EOF
+  cd /home/sandro/lernkurs2022/
+  sudo service nginx restart
+EOF
