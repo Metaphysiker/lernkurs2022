aaf020e98d8230454c4c1c9c47008760854bfb7f
Metaphysiker <s.raess@me.com>
2022-09-17 17:56:27 +0200

    removed heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => "https://lernkurs2022.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://courses.philosophie.ch" }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
