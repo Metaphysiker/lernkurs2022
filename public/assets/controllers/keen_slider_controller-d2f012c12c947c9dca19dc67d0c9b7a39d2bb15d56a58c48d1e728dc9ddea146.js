189070ad0b8887e7eb2765640f622eefb9d8da3c
Metaphysiker <s.raess@me.com>
2022-10-01 00:06:31 +0200

    css fail

--- public/assets/controllers/keen_slider_controller-d2f012c12c947c9dca19dc67d0c9b7a39d2bb15d56a58c48d1e728dc9ddea146.js
+++
@@ -1,1 +0,0 @@
-import{Controller}from"@hotwired/stimulus";export default class extends Controller{static targets=["name","output"];connect(){import("keen-slider").then((e=>{console.log(e);new KeenSlider("#my-keen-slider",{slideChanged(){console.log("slide changed")}})}))}greet(){this.outputTarget.textContent=`Hello, ${this.nameTarget.value}!`}}
