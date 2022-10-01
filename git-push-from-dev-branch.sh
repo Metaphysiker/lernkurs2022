68d0f26b6d37b1aea76a08f5a886873073c6c4c9
Metaphysiker <s.raess@me.com>
2022-10-01 12:13:25 +0200

    before merge

--- git-push-from-dev-branch.sh
+++ git-push-from-dev-branch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+git add .
+git commit -m "before merge"
+git push origin dev
 git checkout main
 git merge dev
 ./deploy-on-digital-ocean-from-main-branch.sh
 git checkout dev
 git merge main
 rails assets:clobber
+git add .
+git commit -m "after merge"
+git push origin dev
 
 echo "finished"
